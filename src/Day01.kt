7c0e0a7022f8bf97531265a5c2cea2ca54f04927
Braiden Judd <braidenj@amazon.com>
2021-12-08 18:06:27 -0600

    Finished Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        if (input.size < 2) return 0
+
+        var last = input[0].toInt()
+        var total = 0
+
+        input.subList(1, input.size).forEach {
+            if (it.toInt() > last) total++
+
+            last = it.toInt()
+        }
+
+        return total
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val windowedList = mutableListOf<String>()
+
+        input.forEachIndexed { i, s ->
+            if (i + 2 < input.size) {
+                windowedList.add(listOf(s, input[i + 1], input[i + 2]).sumOf { it.toInt() }.toString())
+            }
+        }
+
+        return part1(windowedList)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
+    println(part2(testInput))
 
-    val input = readInput("Day01")
+    val input = readInput("day01")
     println(part1(input))
     println(part2(input))
 }
